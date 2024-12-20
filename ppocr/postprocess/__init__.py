09797940c848598946de329320ae60ee58a6f6b4
cluo1989 <chunanluo@126.com>
2024-12-20 17:52:08 +0800

    add build_post_process

---
+++ ppocr/postprocess/__init__.py
@@ -0,0 +1,24 @@
+import copy
+from .rec_postprocess import (
+    CTCDecoder
+)
+
+
+def build_post_process(config, global_config=None):
+    support_dict = {
+        'CTCDecoder': CTCDecoder
+    }
+
+    config = copy.deepcopy(config)
+    module_name = config.pop('name')
+    if module_name == 'None':
+        return
+    if global_config is not None:
+        config.update(global_config)
+
+    assert module_name in support_dict, Exception(
+        'post process only support {}'.format(support_dict)
+    )
+
+    module_class = eval(module_name)(**config)
+    return module_class
